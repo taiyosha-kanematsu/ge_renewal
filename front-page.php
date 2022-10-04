7fa77af0fa55ce04fbcf448fd36c0558347b2eaf
taiyosha-kanematsu <62048023+taiyosha-kanematsu@users.noreply.github.com>
2022-10-04 17:13:34 +0900

    Add files via upload

--- front-page.php
+++ front-page.php
@@ -11,18 +11,18 @@ get_header();
 ?>
 
 <div class="site_contents_container">
-
-
-
-    <article>あ</article>
-    <article>い</article>
-    <article>う</article>
-    <article>え</article>
-    <article>お</article>
-    <article>か</article>
-
-
-
+    <div class="site_contents_inner_wrap">
+        <article class="tokusetsu_container content_container">特設サイト</article>
+        <article class="tokusetsu_container content_container">新着記事</article>
+        <article class="osusume_container content_container">おすすめブック</article>
+        <article class="kouhou_container content_container">広報</article>
+        <article class="kankou_container content_container">観光</article>
+        <article class="gakko_container content_container">学校状況</article>
+        <article class="tokusetsu_container content_container">医療施設・医療情報</article>
+        <article class="sonota_container content_container">その他</article>
+        <article class="jichitai_container content_container">岐阜県自治体＆団体サイト</article>
+        <article class="ebooks_container content_container">イーブックス日本全国で拡大中</article>
+    </div>
 </div>
 
 
